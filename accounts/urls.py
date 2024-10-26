f54368e5beff58b0ba176a8ecbdb905e2446fd32
Yahialqur <ymq2003@gmail.com>
2024-10-26 15:10:13 -0700

    Updated requirements

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,7 +10,7 @@ app_name = "accounts"
 urlpatterns = [
     path("login", views.login_view, name="login"),
     path("register", views.register_user, name="register"),
-    path("logout", views.logout_view, name="logout"),
+    path("logout", views.LogoutView.as_view(), name="logout"),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
