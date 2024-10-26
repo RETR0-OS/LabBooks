7879975b218ad0214bc3e835c75506ee558418c4
RETR0-OS <aadityajindal12@gmail.com>
2024-10-26 15:36:27 -0700

    change logout view to function based view, add in JWT settings

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view, permission_classes
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, logout
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.views import APIView
 from .models import UserProfile
 from course_manager.models import Course
 
@@ -47,9 +45,7 @@ def register_user(request):
         return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
-class LogoutView(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def post(self, request):
-        # JWTs are stateless; client needs to handle token deletion
-        return Response({'message': 'Logged out successfully'}, status=status.HTTP_200_OK)
+@api_view(['GET'])
+def logout_view(request):
+    logout(request)
+    return Response({'message': 'User logged out successfully'}, status=status.HTTP_200_OK)
